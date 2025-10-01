2cae62720d2abe52c29c086b271635e88963f798
ankitkumar0799 <ankitkuswaha07@gmail.com>
2025-10-01 22:20:43 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,45 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  View,
+  Text,
+  Image,
+  Button,
+  TouchableOpacity,
+  Alert,
+  Pressable,
+  StyleSheet,
+} from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const style = {
+ 
+};
 
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+    <SafeAreaView style={styles.container }>
+      <Text
+        style={{
+          color: 'white',
+          fontSize: 50,
+          fontWeight: 600,
+        }}
+      >
+        Apasasdp
+      </Text>
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+      <Image
+        style={{
+          width: 200,
+          height: 300,
+        }}
+        source={{
+          uri: 'https://plus.unsplash.com/premium_photo-1758742688708-57cea1ba9879?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8',
+        }}
       />
-    </View>
+
+      {/* button 1 */}
+      <Button title="button component 1"></Button>
+
+      {/* button 2 */}
+      <TouchableOpacity
+        style={{
+          padding: 10,
+          backgroundColor: 'orange',
+          marginTop: 20,
+        }}
+        onPress={() => Alert.alert('sd')}
+      >
+        <Text>button component 2</Text>
+      </TouchableOpacity>
+
+      {/* button 3 */}
+      <Pressable
+        style={{
+          padding: 10,
+          backgroundColor: 'green',
+          marginTop: 20,
+        }}
+        onPress={() => Alert.alert('sd')}
+      >
+        <Text>button component 3</Text>
+      </Pressable>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#210210',
+    alignItems: 'center',
   },
-});
-
-export default App;
+})
